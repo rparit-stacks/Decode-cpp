eccc5bd47c2ce211e8a9c574cb3ef0b0708ce256
Rohit Parit <rohitparit1934@gmail.com>
2024-06-27 10:53:04 +0530

    ASCAII

--- Adavnce/ascaii.cpp
+++ Adavnce/ascaii.cpp
@@ -1,15 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    for(char i = 65 ; i <= 90; i++)
+    for (char i = 65; i <= 90; i++)
     {
-        int j = (int)i; //typecasting
-        cout<< i << " = " << j << endl;
-        
+        int j = (int)i; // typecasting
+        cout << i << " = " << j << endl;
     }
 
-        
     return 0;
 }
