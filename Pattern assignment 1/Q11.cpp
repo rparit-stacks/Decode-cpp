f77c905bb3046de8d41ff5bb71e90ea7a0f45edb
Rohit Parit <rohitparit1934@gmail.com>
2024-07-09 19:10:34 +0530

    patteran 11

--- Pattern assignment 1/Q11.cpp
+++ Pattern assignment 1/Q11.cpp
@@ -1,38 +1,39 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
     int num;
-    cout<<"Enter a number: ";
-    cin>>num;
-    int flag = num, count = num-1, n = num;
-    for(int i = 1; i <= ((num*2)-1); i++)
-    // {
-    //     if(i<=num) 
-    //     {
-    //         for(int l = 1; l <= count; l++) cout<<" ";
-    //         for(int j = 1; j <= i; j++) cout<<"*";
-    //     }
+    cout << "Enter a number: ";
+    cin >> num;
+    int flag = num, count = num - 1, n = num;
+    for (int i = 1; i <= ((num * 2) - 1); i++)
+    {
+        if (i <= num)
+        {
+            for (int l = 1; l <= count; l++)
+                cout << " ";
+            for (int j = 1; j <= i; j++)
+                cout << "*";
+        }
 
-    //     else if(i>num)
-    //     {
-    //         for(int z = 1; z <= num-1 ; z++)
-    //         {
-    //             for(int x = 1; x<=z; x++)cout<<" ";
-    //             for(int y = 1; y<=n-1; y++) cout<<"*";
-    //             n--;
-    //              cout<<endl;
-    //         }
-           
-    //     }
-    
-    //     cout<<endl;
-    //     count--;
-        
-    // }
-    
+        else if (i > num)
+        {
+            for (int z = 1; z <= num - 1; z++)
+            {
+                for (int x = 1; x <= z; x++)
+                    cout << " ";
+                for (int y = 1; y <= n - 1; y++)
+                    cout << "*";
+                n--;
+                cout << endl;
+            }
+        }
 
-    for
+        cout << endl;
+        count--;
+    }
+
     return 0;
 }
